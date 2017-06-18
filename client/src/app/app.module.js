d8e6f7526c6345a22ee1296303d45596ad549789
AmitDebadwar <amit.debadwar@gmail.com>
2017-06-18 23:53:28 +0530

    sai

---
+++ client/src/app/app.module.js
@@ -0,0 +1,36 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var router_1 = require("@angular/router");
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var forms_1 = require("@angular/forms");
+var app_component_1 = require("./app.component");
+var header_component_1 = require("./components/Header/header.component");
+var tasklist_component_1 = require("./components/TaskList/tasklist.component");
+var login_component_1 = require("./components/Login/login.component");
+var newpatientcreation_component_1 = require("./components/NewPatientCreation/newpatientcreation.component");
+var appRoutes = [
+    { path: 'login', component: login_component_1.LoginComponent },
+    { path: 'allTasks', component: tasklist_component_1.TaskListComponent },
+    { path: 'newPatientCreation', component: newpatientcreation_component_1.NewPatientCreationComponent },
+    { path: '**', component: login_component_1.LoginComponent }
+];
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    core_1.NgModule({
+        imports: [platform_browser_1.BrowserModule, router_1.RouterModule.forRoot(appRoutes), forms_1.FormsModule],
+        declarations: [app_component_1.AppComponent, header_component_1.HeaderComponent, login_component_1.LoginComponent, tasklist_component_1.TaskListComponent, newpatientcreation_component_1.NewPatientCreationComponent],
+        bootstrap: [app_component_1.AppComponent]
+    })
+], AppModule);
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { RouterModule, Routes }        from '@angular/router'; 
+import { NgModule }                    from '@angular/core';
+import { BrowserModule }               from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent }                from './app.component';
+import { HeaderComponent}              from './components/Header/header.component';
+import { TaskListComponent }           from './components/TaskList/tasklist.component';
+import { LoginComponent }              from './components/Login/login.component';
+import { NewPatientCreationComponent } from './components/NewPatientCreation/newpatientcreation.component';
+
+ const appRoutes:Routes=[
+   {path:'login', component:LoginComponent},
+   {path:'allTasks', component: TaskListComponent},
+   {path:'newPatientCreation', component: NewPatientCreationComponent},
+   {path:'**', component:LoginComponent}
+ ];
+
+
+@NgModule({
+  imports:      [ BrowserModule, RouterModule.forRoot(appRoutes),FormsModule ],
+  declarations: [ AppComponent, HeaderComponent, LoginComponent, TaskListComponent, NewPatientCreationComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
